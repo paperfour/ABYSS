0384cde3d60aaf04cf063bfd259bdce3dc06efb6
NotUnfound <82277745+NotUnfound@users.noreply.github.com>
2025-02-05 16:51:46 -0500

    testing playground, sounds, music

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 
+@onready var WalkSound = $WalkSound
 
 const SPEED = 100.0
 const JUMP_VELOCITY = -200.0
 
+var step_cooldown = 100
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -11,15 +15,28 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
+	# 1st = -1, 2nd = 1, both/none = 0
+
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+
+	# If there is a direction to go
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * SPEED		
+		
+		if not WalkSound.playing:
+			WalkSound.play()
+	# If there is no player movement
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED/15)
+		WalkSound.stop()
 
 	move_and_slide()
